64693f15f074baa4fe674962e1f9ca1d80e33eb8
Mateusz Paprocki <mattpap@gmail.com>
2013-11-06 22:20:41 +0100

    Clean up and simplify IPythonRemoteData

--- bokeh/pandasobjects.py
+++ bokeh/pandasobjects.py
@@ -7,9 +7,9 @@ import requests
 
 from . import protocol
 
-from .properties import (HasProps, MetaHasProps, 
+from .properties import (HasProps, MetaHasProps,
         Any, Dict, Enum, Float, Instance, Int, List, String,
-        Color, Pattern, Percent, Size, Bool)
+        Color, Percent, Size, Bool)
 
 from .objects import PlotObject, Plot, ColumnDataSource
 from .session import PlotContext, PlotList
@@ -24,80 +24,60 @@ class IPythonRemoteData(PlotObject):
     varname = String()
     computed_columns = List()
     metadata = Dict()
-    
+
     #hack... we're just using this field right now to trigger events
     selected = Int(0)
-    data = Int(0)    
-    
-    def setselect(self, select, transform):
-                
+    data = Int(0)
+
+    def _url(self, func=None):
         remotedata = self
-        url = "http://%s:%s/array/%s/setselect" % (remotedata.host,
-                                                remotedata.port,
-                                                remotedata.varname)
-        data = transform
-        data['selected'] = select
-        requests.post(url, data=protocol.serialize_json(data))
+        func = "/" + func if func is not None else ""
+        url = "http://%s:%s/array/%s%s" % \
+            (remotedata.host, remotedata.port, remotedata.varname, func)
+        return url
+
+    def _trigger_events(self):
         self.selected = self.selected + 1
-        
-    def search(self, search):
-        remotedata = self
-        url = "http://%s:%s/array/%s/search" % (remotedata.host,
-                                                remotedata.port,
-                                                remotedata.varname)
-        requests.post(url, data=search)
-        self.selected = self.selected + 1        
-        
-    def select(self, select, transform):
-                
-        remotedata = self
-        url = "http://%s:%s/array/%s/select" % (remotedata.host,
-                                                remotedata.port,
-                                                remotedata.varname)
+
+    def setselect(self, select, transform):
         data = transform
         data['selected'] = select
-        requests.post(url, data=protocol.serialize_json(data))
-        self.selected = self.selected + 1        
-        
+        requests.post(self._url("setselect"), data=protocol.serialize_json(data))
+        self._trigger_events()
+
+    def search(self, search):
+        requests.post(self._url("search"), data=search)
+        self._trigger_events()
+
+    def select(self, select, transform):
+        data = transform
+        data['selected'] = select
+        requests.post(self._url("select"), data=protocol.serialize_json(data))
+        self._trigger_events()
+
     def deselect(self, deselect, transform):
-        remotedata = self
-        url = "http://%s:%s/array/%s/deselect" % (remotedata.host,
-                                                  remotedata.port,
-                                                  remotedata.varname)
         data = transform
         data['selected'] = deselect
-        requests.post(url, data=protocol.serialize_json(data))
-        self.selected = self.selected + 1        
-        
+        requests.post(self._url("selected"), data=protocol.serialize_json(data))
+        self._trigger_events()
+
     def get_data(self, transform):
-        remotedata = self
-        url = "http://%s:%s/array/%s" % (remotedata.host,
-                                         remotedata.port,
-                                         remotedata.varname)
-        data = requests.get(url, data=protocol.serialize_json(transform)).json()
+        data = requests.get(self._url(), data=protocol.serialize_json(transform)).json()
         self.metadata = data.pop('metadata', {})
         return data
-    
+
     def set_computed_columns(self, computed_columns):
-        
-        remotedata = self
-        url = "http://%s:%s/array/%s/computed" % (remotedata.host,
-                                                  remotedata.port,
-                                                  remotedata.varname)
-        data = requests.get(
-            url,
-            data=protocol.serialize_json(computed_columns)).json()
+        data = requests.get(self._url("computed"), data=protocol.serialize_json(computed_columns)).json()
         self.computed_columns = computed_columns
         self.data += 1
         return data
-        
-    
+
 class PandasPlotSource(ColumnDataSource):
     source = Instance(has_ref=True)
-    
+
     def __init__(self, *args, **kwargs):
         super(PandasPlotSource, self).__init__(*args, **kwargs)
-        
+
     def setup_events(self):
         self.on_change('selected', self, 'selection_callback')
         self.source.on_change('selected', self, 'get_data')
@@ -108,22 +88,22 @@ class PandasPlotSource(ColumnDataSource):
 
     def selection_callback(self, obj=None, attrname=None, old=None, new=None):
         self.setselect(self.selected)
-        
+
     def transform(self):
         return {}
-    
+
     def setselect(self, select):
         self.source.setselect(select, self.transform())
         self.get_data()
-        
+
     def select(self, select):
         self.source.select(select, self.transform())
         self.get_data()
-        
+
     def deselect(self, deselect):
         self.source.deselect(deselect, self.transform())
         self.get_data()
-        
+
     def get_data(self, obj=None, attrname=None, old=None, new=None):
         data = self.source.get_data(self.transform())
         #ugly:
@@ -132,8 +112,8 @@ class PandasPlotSource(ColumnDataSource):
         self.totallength = data.pop('totallength')
         self.column_names = data['column_names']
         self.data = data['data']
-    
-    
+
+
 class PandasPivotTable(PlotObject):
     source = Instance(has_ref=True)
     sort = List()
@@ -141,7 +121,7 @@ class PandasPivotTable(PlotObject):
     offset = Int(default=0)
     length = Int(default=100)
     maxlength = Int()
-    totallength = Int()    
+    totallength = Int()
     precision = Dict()
     tabledata = Dict()
     filterselected = Bool(default=False)
@@ -157,7 +137,7 @@ class PandasPivotTable(PlotObject):
         self.source.on_change('computed_columns', self, 'get_data')
         if not self.tabledata:
             self.get_data()
-        
+
     def format_data(self, jsondata):
         """inplace manipulation of jsondata
         """
@@ -175,7 +155,7 @@ class PandasPivotTable(PlotObject):
                     data[idx] = time.strftime("%Y-%m-%d %H:%M:%S", timeobj)
                 if isinstance(val, float):
                     data[idx] = "%%.%df" % precision.get(colname,2)%data[idx]
-    
+
     def transform(self):
         return dict(sort=self.sort,
                     group=self.group,
@@ -183,19 +163,19 @@ class PandasPivotTable(PlotObject):
                     length=self.length,
                     filterselected=self.filterselected,
                     )
-    
+
     def setselect(self, select):
         self.source.setselect(select, self.transform())
         self.get_data()
-        
+
     def select(self, select):
         self.source.select(select, self.transform())
         self.get_data()
-        
+
     def deselect(self, deselect):
         self.source.deselect(deselect, self.transform())
         self.get_data()
-        
+
     def get_data(self, obj=None, attrname=None, old=None, new=None):
         data = self.source.get_data(self.transform())
         print(data['data']['_selected'])
@@ -203,4 +183,4 @@ class PandasPivotTable(PlotObject):
         self.totallength = data.pop('totallength')
         self.format_data(data['data'])
         self.tabledata = data
-        
+
